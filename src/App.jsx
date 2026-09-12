dab81a992ab93b7de9c761b2875f86f78a7293dd
Valeriat92 <valeriatrevisi7@gmail.com>
2026-09-12 15:17:42 -0300

    Agrega dashboard de cliente y contexto de autenticacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './Views/Home/Home';
 import Services from "./Views/Services/Services";
+import ClientDashboard from "./Views/ClientDashboard/ClientDashboard";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<Services />} />
+      <Route path="/dashboard" element={<ClientDashboard />} />
     </Routes>
   );
 }
