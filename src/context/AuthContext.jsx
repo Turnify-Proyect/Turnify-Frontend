415a6fbd6dbfe667168f7855353e4b6971483fb8
Valeriat92 <valeriatrevisi7@gmail.com>
2026-09-17 23:54:13 -0300

    Agrega dashboard de administrador y ajusta autenticacion por roles

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,11 +2,30 @@ import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
+function decodeToken(token) {
+  if (!token) return null;
+
+  try {
+    const payload = token.split(".")[1];
+
+    const decodedPayload = atob(
+      payload.replace(/-/g, "+").replace(/_/g, "/")
+    );
+
+    return JSON.parse(decodedPayload);
+  } catch (error) {
+    console.error("Error al decodificar el token:", error);
+    return null;
+  }
+}
+
+
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(() => {
     return localStorage.getItem("token");
   });
 
+  const user = decodeToken(token);
   const login = (newToken) => {
     localStorage.setItem("token", newToken);
     setToken(newToken);
@@ -21,6 +40,7 @@ export function AuthProvider({ children }) {
     <AuthContext.Provider
       value={{
         token,
+        user,
         isAuthenticated: !!token,
         login,
         logout,
